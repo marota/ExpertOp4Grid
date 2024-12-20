22a508073a6043b72bcea4d170f66e413e0bc913
marota <antoine-marot@live.fr>
2024-12-20 10:57:15 +0100

    Update setup.py
    Slight version update on 0.2.3

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ pkgs = {
 }
 
 setup(name='ExpertOp4Grid',
-      version='0.2.3',
+      version='0.2.3_post1',
       description='Expert analysis algorithm for solving overloads in a powergrid',
       long_description_content_type="text/markdown",
       classifiers=[
