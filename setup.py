814ee17fdd370d38a4256a33f78c7bc063d8a198
marota <antoine-marot@live.fr>
2025-12-08 21:47:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ pkgs = {
 }
 
 setup(name='ExpertOp4Grid',
-      version='0.2.7_post2',
+      version='0.2.8',
       description='Expert analysis algorithm for solving overloads in a powergrid',
       long_description_content_type="text/markdown",
       classifiers=[
